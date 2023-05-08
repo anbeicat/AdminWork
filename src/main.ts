2067b4884443f159d20f05b4edf8e32fd3aba83b
anqiao <1102877041@qq.com>
2023-05-08 13:52:09 +0800

    fix: 打包

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: anqiao 1102877041@qq.com
  * @Date: 2023-03-02 11:23:52
  * @LastEditors: anqiao 1102877041@qq.com
- * @LastEditTime: 2023-05-04 13:28:17
+ * @LastEditTime: 2023-05-08 13:51:37
  * @description:
  * @FilePath: /AdminWork/src/main.ts
  */
@@ -22,12 +22,9 @@ import { useDefaultStore } from "./store/defaultSettings"
 const app = createApp(App)
 
 
+
 // 挂载pinia
 app.use(store)
-const defaultStore = useDefaultStore()
-localStorage['userInfo'] = localStorage['userInfo'] || JSON.stringify(defaultStore)
-console.log('defaultStore',defaultStore,localStorage['userInfo']);
-
 app.use(router)
 app.config.globalProperties.axios = 'axios';
 router.beforeEach(async (to, from, next) => {
@@ -37,7 +34,10 @@ router.beforeEach(async (to, from, next) => {
     }
     next()
 })
-
+const defaultStore = useDefaultStore()
+localStorage.setItem('userInfo', localStorage.getItem('userInfo') || JSON.stringify(defaultStore))
+// localStorage['userInfo'] = localStorage['userInfo'] || JSON.stringify(defaultStore)
+console.log('defaultStore',defaultStore,localStorage.getItem('userInfo'));
 
 // 挂载实例
 app.mount('#app');
